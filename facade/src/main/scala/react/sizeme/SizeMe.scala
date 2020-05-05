489dd28e1b16742909bb90b66694db8d3eb01557
Carlos Quiroz <carlos.m.quiroz@gmail.com>
2020-05-04 20:09:14 -0400

    Add property to optionally rerender
    Signed-off-by: Carlos Quiroz <carlos.m.quiroz@gmail.com>

--- facade/src/main/scala/react/sizeme/SizeMe.scala
+++ facade/src/main/scala/react/sizeme/SizeMe.scala
@@ -18,6 +18,7 @@ final case class SizeMe(
   refreshMode:            js.UndefOr[RefreshMode],
   placeholder:            js.UndefOr[VdomNode],
   noPlaceholder:          js.UndefOr[Boolean],
+  renderOnUndefined:      Boolean,
   override val modifiers: Seq[TagMod]
 ) extends GenericComponentPA[SizeMe.SizeMeProps, SizeMe] {
   override def cprops              = SizeMe.props(this)
@@ -25,7 +26,7 @@ final case class SizeMe(
   override def addModifiers(modifiers: Seq[TagMod]) = copy(modifiers = this.modifiers ++ modifiers)
 }
 
-object SizeMe {
+object SizeMe                                              {
 
   @js.native
   @JSImport("react-sizeme", "SizeMe")
@@ -52,43 +53,52 @@ object SizeMe {
              q.refreshRate,
              q.refreshMode,
              q.placeholder,
-             q.noPlaceholder)
+             q.noPlaceholder,
+             q.renderOnUndefined
+    )
 
   def rawprops(
-    children:        RenderF,
-    monitorWidth:    js.UndefOr[Boolean] = js.undefined,
-    monitorHeight:   js.UndefOr[Boolean] = js.undefined,
-    monitorPosition: js.UndefOr[Boolean] = js.undefined,
-    refreshRate:     js.UndefOr[Int] = js.undefined,
-    refreshMode:     js.UndefOr[RefreshMode] = js.undefined,
-    placeholder:     js.UndefOr[VdomNode] = js.undefined,
-    noPlaceholder:   js.UndefOr[Boolean] = js.undefined
+    children:          RenderF,
+    monitorWidth:      js.UndefOr[Boolean] = js.undefined,
+    monitorHeight:     js.UndefOr[Boolean] = js.undefined,
+    monitorPosition:   js.UndefOr[Boolean] = js.undefined,
+    refreshRate:       js.UndefOr[Int] = js.undefined,
+    refreshMode:       js.UndefOr[RefreshMode] = js.undefined,
+    placeholder:       js.UndefOr[VdomNode] = js.undefined,
+    noPlaceholder:     js.UndefOr[Boolean] = js.undefined,
+    renderOnUndefined: Boolean
   ): SizeMeProps = {
     val p = (new js.Object).asInstanceOf[SizeMeProps]
-    p.children = (s: SizeP) =>
-      if (!js.isUndefined(s) && !js.isUndefined(s.size.width)) children(s.size).rawNode
+    p.children = (s: SizeP) => {
+      println(s)
+      println("-----")
+      println(s.size)
+      if (renderOnUndefined || (!js.isUndefined(s) && !js.isUndefined(s.size.width)))
+        children(s.size).rawNode
       else placeholder.rawNode
-    p.monitorWidth    = monitorWidth
-    p.monitorHeight   = monitorHeight
-    p.monitorPosition = monitorPosition
-    p.refreshRate     = refreshRate
-    p.refreshMode     = refreshMode.toJs
-    p.noPlaceholder   = noPlaceholder
+    }
+    monitorWidth.foreach(v => p.monitorWidth = v)
+    monitorHeight.foreach(v => p.monitorHeight = v)
+    monitorPosition.foreach(v => p.monitorPosition = v)
+    refreshRate.foreach(v => p.refreshRate = v)
+    refreshMode.toJs.foreach(v => p.refreshMode = v)
+    noPlaceholder.foreach(v => p.noPlaceholder = v)
     p
   }
 
   val component = JsComponent[SizeMeProps, Children.None, Null](RawComponent)
 
   def apply(
-    monitorWidth:    js.UndefOr[Boolean] = js.undefined,
-    monitorHeight:   js.UndefOr[Boolean] = js.undefined,
-    monitorPosition: js.UndefOr[Boolean] = js.undefined,
-    refreshRate:     js.UndefOr[Int] = js.undefined,
-    refreshMode:     js.UndefOr[RefreshMode] = js.undefined,
-    placeholder:     js.UndefOr[VdomNode] = js.undefined,
-    noPlaceholder:   js.UndefOr[Boolean] = js.undefined,
-    modifiers:       Seq[TagMod] = Seq.empty
-  )(render:          RenderF): SizeMe =
+    monitorWidth:      js.UndefOr[Boolean] = js.undefined,
+    monitorHeight:     js.UndefOr[Boolean] = js.undefined,
+    monitorPosition:   js.UndefOr[Boolean] = js.undefined,
+    refreshRate:       js.UndefOr[Int] = js.undefined,
+    refreshMode:       js.UndefOr[RefreshMode] = js.undefined,
+    placeholder:       js.UndefOr[VdomNode] = js.undefined,
+    noPlaceholder:     js.UndefOr[Boolean] = js.undefined,
+    renderOnUndefined: Boolean = false,
+    modifiers:         Seq[TagMod] = Seq.empty
+  )(render:            RenderF): SizeMe =
     new SizeMe(render,
                monitorWidth,
                monitorHeight,
@@ -97,5 +107,7 @@ object SizeMe {
                refreshMode,
                placeholder,
                noPlaceholder,
-               modifiers)
+               renderOnUndefined,
+               modifiers
+    )
 }
